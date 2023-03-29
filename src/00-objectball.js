2e367f39008f1a84714f92b7ea75dabdca280456
Hoperaisers <abdallahsafari@gmail.com>
2023-03-29 11:28:45 +0300

    initial commit

---
+++ src/00-objectball.js
@@ -0,0 +1,25 @@
+console.log(gameObject());
+/*
+{
+  home: {
+    teamName: "",
+    colors: [...],
+    players: {
+      "Alan Anderson": {...},
+      "Reggie Evans": {...}
+    }
+  },
+  away: {
+    ...
+  } 
+}
+*/
+
+function homeTeamName() {
+    let object = gameObject()
+    return object['home']['teamName']
+}
+
+console.log(homeTeamName())
+  // logs "Brooklyn Nets"
+
